aa97d363a2a317f3c6c71b8e73b678e4ac9bcb11
Vladimir Shefer <quameu@gmail.com>
2017-11-12 01:43:01 +0300

    1c6t javadoc added

--- src/main/java/com/epam/javaschool/shefer/chapter1/task6/Notebook.java
+++ src/main/java/com/epam/javaschool/shefer/chapter1/task6/Notebook.java
@@ -3,7 +3,7 @@ package com.epam.javaschool.shefer.chapter1.task6;
 import java.util.*;
 
 /**
- * Notebook
+ * Notebook is a collection of Notes
  */
 public class Notebook {
 
@@ -11,16 +11,18 @@ public class Notebook {
         Notebook notebook = new Notebook();
         notebook.add("hello world");
         notebook.add(new Note("papaia is very delicious"));
-        notebook.printAllNotes();
+        notebook.printAll();
         notebook.redactNote(1);
-        notebook.printAllNotes();
+        notebook.printAll();
         notebook.deleteNote(0);
-        notebook.printAllNotes();
+        notebook.printAll();
     }
 
-    private ArrayList<Note> notes = new ArrayList<>();
-
-    private String readText() {
+    /**
+     * Reads from console untill user enters `###`
+     * @return entered text
+     */
+    private static String readText() {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         System.out.println("Enter your text below. To commit enter ###");
@@ -31,12 +33,35 @@ public class Notebook {
         return sb.toString();
     }
 
-    public void printAllNotes() {
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    /**
+     * @param id - index of note in list
+     * @throws IndexOutOfBoundsException if id less than 0 or more than size
+     */
+    public void redactNote(int id) throws IndexOutOfBoundsException {
+        Note note = notes.get(id);
+        System.out.println(note);
+        note.setText(readText());
+    }
+
+    public int size(){
+        return notes.size();
+    }
+
+    /**
+     * prints all notes
+     */
+    public void printAll() {
         for (int i = 0; i < notes.size(); i++) {
             Note note = notes.get(i);
             System.out.println("Note ID " + i);
             System.out.println(note);
         }
+    }
+
+    public void print(int id){
+        System.out.println((notes.get(id)));
     }
 
     public void add(Note note) {
@@ -48,15 +73,9 @@ public class Notebook {
     }
 
     /**
-     * @param id index of note in list
+     * @param id - index of note in list
      * @throws IndexOutOfBoundsException if id less than 0 or more than size
      */
-    public void redactNote(int id) throws IndexOutOfBoundsException {
-        Note note = notes.get(id);
-        System.out.println(note);
-        note.setText(readText());
-    }
-
     public void deleteNote(int id) throws IndexOutOfBoundsException {
         notes.remove(id);
     }
