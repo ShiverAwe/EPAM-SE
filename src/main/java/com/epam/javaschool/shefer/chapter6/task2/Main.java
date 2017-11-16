374862a397931cdb9cd9dc6325077e84278c08c8
Vladimir Shefer <quameu@gmail.com>
2017-11-17 01:45:24 +0300

    c6t2 minor

--- src/main/java/com/epam/javaschool/shefer/chapter6/task2/Main.java
+++ src/main/java/com/epam/javaschool/shefer/chapter6/task2/Main.java
@@ -1,14 +1,16 @@
 package com.epam.javaschool.shefer.chapter6.task2;
 
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
-//        String line = "a\\nb\\tc\\tde";
-//        line = line.replaceAll("(\\\\n)", "\n");
-//        line = line.replaceAll("(\\\\t)", "\t");
-//        line = line.replaceAll("(\\\\r)", "\r");
-//        System.out.println(line);
-
         Properties pr = new Properties();
-        pr.parse("hi\\ po = k   ksl \\t sdfkj sdflk \\n skd            fj sdlfk");
+        //InputStream input = Main.class.getResourceAsStream("/chapter6/task2/file.properties");
+        //InputStream input = Main.class.getClass().getResourceAsStream("/chapter6/task2/file.properties");
+       // pr.load(input);
+       // pr.addFromLine("hi\\ po = k   ksl \\t sdfkj sdflk \\n skd            fj sdlfk");
+        for (String key: pr.keySet()){
+            System.out.println(key + " : " + pr.get(key));
+        }
     }
 }
--- src/main/java/com/epam/javaschool/shefer/chapter6/task2/Properties.java
+++ src/main/java/com/epam/javaschool/shefer/chapter6/task2/Properties.java
@@ -1,11 +1,10 @@
 package com.epam.javaschool.shefer.chapter6.task2;
 
-import com.sun.istack.internal.NotNull;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,58 +12,71 @@ public class Properties {
 
     Map<String, String> properties = new HashMap<>();
 
-    public void load(File file){
-        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(file)))){
-            StringBuilder sb = new StringBuilder();
-            while (sc.hasNext()){
+    public void load(InputStream input) {
+        try (Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(input)))) {
+            while (sc.hasNext()) {
                 String s = sc.nextLine();
 
                 // Ignoring comments
-                if (s.charAt(0)=='!' || s.charAt(0)=='#') continue;
+                if (s.charAt(0) == '!' || s.charAt(0) == '#') continue;
 
                 // Concatenating backslash-separated lines
-                while (s.charAt(s.length()-1) == '\\') {
+                while (s.charAt(s.length() - 1) == '\\') {
                     if (sc.hasNext()) {
                         s += sc.nextLine();
                     } else {
                         throw new RuntimeException(new EOFException());
                     }
                 }
-                parse(s);
+                addFromLine(s);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
 
-
-    public void parse(String line){
-        Pattern p = Pattern.compile(
-                "^((?:(?:\\\\[ ]?)|[\\w])+)(?:\\s*[=:]\\s*)(.+)$"
-        );
+    public void addFromLine(String line) {
+        Pattern p = Pattern.compile("^((?:(?:[\\\\][ ])|[\\w])+)(?:[\\s]*[=:][\\s]*)(.*)$");
 
         Matcher m = p.matcher(line);
-
         if (!m.matches()) return;
 
         String key = safeKey(m.group(1));
         String value = safeValue(m.group(2));
 
-        System.out.println(key + " : " + value);
-
+        //System.out.println(key + " : " + value);
+        properties.put(key, value);
     }
 
-    private static String safeValue(String line){
-        line = line.replaceAll("\\s+", " ");
-        line = line.replaceAll("(\\\\n)", "\n");
-        line = line.replaceAll("(\\\\t)", "\t");
-        line = line.replaceAll("(\\\\r)", "\r");
+    private static String safeValue(String line) {
+        line = line.replaceAll("[\\s]+", " ");
+        line = line.replaceAll("([^\\\\][\\\\]n)", "\n");
+        line = line.replaceAll("([^\\\\][\\\\]t)", "\t");
+        line = line.replaceAll("([^\\\\][\\\\]r)", "\r");
+        line = line.replaceAll("([\\\\][\\\\])", "\\\\");
         return line;
     }
 
-    private static String safeKey(String line){
+    private static String safeKey(String line) {
         line = line.replaceAll("(\\\\ )", " ");
         return line;
     }
 
+    public int size() {
+        return properties.size();
+    }
+
+    public boolean isEmpty() {
+        return properties.isEmpty();
+    }
+
+    public String get(String key) {
+        return properties.get(key);
+    }
+
+    public String put(String key, String value) {
+        return properties.put(key, value);
+    }
+
+    public Set<String> keySet() {
+        return properties.keySet();
+    }
 }
